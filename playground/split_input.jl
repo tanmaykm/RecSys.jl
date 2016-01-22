cf26733783e2216bd4c00a34bb06928b5491f667
tan <tanmaykm@gmail.com>
2016-01-23 00:00:04 +0530

    introduce chunks

---
+++ playground/split_input.jl
@@ -0,0 +1,128 @@
+using RecSys
+include("/home/tan/Work/julia/packages/RecSys/src/chunk.jl")
+
+function split_sparse(S, chunkmax, filepfx)
+    metafilename = "$(filepfx).meta"
+    open(metafilename, "w") do mfile
+        chunknum = 1
+        count = 1
+        colstart = 1
+        splits = UnitRange[]
+        nzval = S.nzval
+        rowval = S.rowval
+        for col in 1:size(S,2)
+            npos = S.colptr[col+1]
+            if npos >= (count + chunkmax)
+                print("\tchunk $chunknum ... ")
+                cfilename = "$(filepfx).$(chunknum)"
+                println(mfile, colstart, ",", col-1, ",", cfilename)
+                open(cfilename, "w") do cfile
+                    for cidx in colstart:(col-1)
+                        rowstart = S.colptr[cidx]
+                        rowend = S.colptr[cidx+1]-1
+                        for ridx in rowstart:rowend
+                            println(cfile, rowval[ridx], ",", cidx, ",", nzval[ridx])
+                        end
+                    end
+                end
+                #    SC = sub(S, :, colstart:(col-1))
+                #    R,C,NZ = findnz(SC)
+                #    print("... ")
+                #    for idx in 1:length(R)
+                #        println(cfile, R[idx], ",", C[idx], ",", NZ[idx])
+                #    end
+                #end
+                push!(splits, colstart:(col-1))
+                colstart = col
+                count = npos
+                chunknum += 1
+                println("done")
+            end
+        end
+        println("splits: $splits")
+    end
+    nothing
+end
+
+function splitall(inp::DlmFile, output_path::AbstractString, nsplits::Int)
+    println("reading inputs...")
+    ratings = RecSys.read_input(inp)
+
+    users   = convert(Vector{Int64},   ratings[:,1]);
+    items   = convert(Vector{Int64},   ratings[:,2]);
+    ratings = convert(Vector{Float64}, ratings[:,3]);
+    R = sparse(users, items, ratings);
+    nratings = length(ratings)
+    nsplits_u = round(Int, nratings/nsplits)
+    nsplits_i = round(Int, nratings/nsplits)
+
+    println("splitting R itemwise at $nsplits_i items...")
+    split_sparse(R, nsplits_i, joinpath(output_path, "R_itemwise"))
+    RT = R'
+    println("splitting RT userwise at $nsplits_u users...")
+    split_sparse(RT, nsplits_u, joinpath(output_path, "RT_userwise"))
+    nothing
+end
+
+function split_movielens(dataset_path = "/data/Work/datasets/movielens/ml-20m")
+    ratings_file = DlmFile(joinpath(dataset_path, "ratings.csv"); dlm=',', header=true)
+    splitall(ratings_file, joinpath(dataset_path, "splits"), 10)
+end
+
+function split_lastfm(dataset_path = "/data/Work/datasets/last_fm_music_recommendation/profiledata_06-May-2005")
+    ratings_file = DlmFile(joinpath(dataset_path, "user_artist_data.txt"))
+    splitall(ratings_file, joinpath(dataset_path, "splits"), 20)
+end
+
+function load_splits(dataset_path = "/data/Work/datasets/last_fm_music_recommendation/profiledata_06-May-2005/splits")
+    cf = ChunkedSparseMatrixCSC(joinpath(dataset_path, "R_itemwise.meta"))
+    println(cf)
+    nchunks = length(cf.chunks)
+    for idx in 1:10
+        cid = floor(Int, nchunks*rand()) + 1
+        println("fetching from chunk $cid")
+        c = cf.chunks[cid]
+        key = floor(Int, length(c.keyrange)*rand()) + c.keyrange.start
+        println("fetching key $key")
+        r,v = data(cf, key)
+        #println("\tr:$r, v:$v")
+    end
+    println("finished")
+end
+
+function create_memmapped_splits(dataset_path = "/data/Work/datasets/last_fm_music_recommendation/profiledata_06-May-2005/splitsmem")
+    metafilename = joinpath(dataset_path, "mem.meta")
+    N = 3
+    M = 100
+    sz = M*N*sizeof(Float64)
+    NC = 10
+
+    touch(metafilename)
+    cf = ChunkedMemMappedMatrix(metafilename, N)
+
+    for idx in 1:NC
+        chunkfname = joinpath(dataset_path, "mem.$idx")
+        r1 = (idx-1)*M+1
+        r2 = idx*M
+        chunk = Chunk(chunkfname, 0, sz, r1:r2, MemMappedMatrix{Float64,N})
+        push!(cf.chunks, chunk)
+    end
+    for idx in 1:NC
+        A = data(cf, idx*M)
+        fill!(A, idx)
+    end
+    for idx in 1:NC
+        A = data(cf, idx*M)
+        println(A[1])
+    end
+
+    writemeta(cf)
+    cf = ChunkedMemMappedMatrix(metafilename, N)
+    for idx in 1:NC
+        A = data(cf, idx*M)
+        println(A[1])
+    end
+end
+
+#load_splits()
+create_memmapped_splits()
---
+++ src/chunk.jl
@@ -0,0 +1,135 @@
+# Chunks are parts of a large data datastructure that can be:
+#   - loaded on demand
+#   - cached, but evicted on memory pressure
+#   - optionally attached with locality for efficient IO (in future)
+# A chunked files:
+#   - has a metadata section that lists location and data range of each chunk
+#   - chunk key type (data range) must have the `in`, `first`, `last`, and `length` methods defined
+
+using LRUCache
+using Base.Mmap
+
+type MemMappedMatrix{T,N}
+    val::Matrix{T}
+end
+
+type Chunk{K,V}
+    path::AbstractString
+    offset::Int
+    size::Int
+    keyrange::K
+    valtype::Type{V}
+    data::Nullable{WeakRef}
+end
+
+function Chunk(path::AbstractString, offset::Integer, size::Integer, keyrange, V)
+    K = typeof(keyrange)
+    Chunk{K,V}(path, offset, size, keyrange, V, Nullable{WeakRef}())
+end
+
+function Chunk(path::AbstractString, keyrange, V)
+    size = filesize(path)
+    K = typeof(keyrange)
+    Chunk{K,V}(path, 0, size, keyrange, V, Nullable{WeakRef}())
+end
+
+function load{T,N}(::Type{MemMappedMatrix{T,N}}, chunk::Chunk)
+    println("loading memory mapping chunk $(chunk.path)")
+    ncells = div(chunk.size, sizeof(T))
+    M = Int(ncells/N)
+    A = Mmap.mmap(chunk.path, Matrix{T}, (M,N), chunk.offset)
+    MemMappedMatrix{T,N}(A)
+end
+
+function load{T<:Vector{UInt8}}(::Type{T}, chunk::Chunk)
+    println("loading chunk $(chunk.path)")
+    open(chunk.path) do f
+        seek(f, chunk.offset)
+        databytes = Array(UInt8, chunk.size)
+        read!(f, databytes)
+        return databytes::T
+    end
+end
+
+function load{T<:Matrix}(::Type{T}, chunk::Chunk)
+    M = readcsv(load(Vector{UInt8}, chunk))
+    M::T
+end
+
+function load{T<:SparseMatrixCSC}(::Type{T}, chunk::Chunk)
+    A = load(Matrix{Float64}, chunk)
+    rows = convert(Vector{Int64},   A[:,1]);
+    cols = convert(Vector{Int64},   A[:,2]);
+    vals = convert(Vector{Float64}, A[:,3]);
+
+    # subtract keyrange to keep sparse matrix small
+    cols .-= (first(chunk.keyrange) - 1)
+    sparse(rows, cols, vals)::T
+end
+
+function data{K,V}(chunk::Chunk{K,V}, lrucache::LRU)
+    if isnull(chunk.data)
+        data = load(V, chunk)
+        finalizer(data, x->finalize_chunk_data(chunk, x))
+        chunk.data = WeakRef(data)
+    end
+    v = get(chunk.data).value
+    lrucache[chunk.keyrange] = v
+    v::V
+end
+
+function finalize_chunk_data(chunk::Chunk, data)
+    println("unloading chunk $(chunk.path)")
+    chunk.data = nothing
+end
+
+type ChunkedFile{K,V}
+    keyrangetype::Type{K}
+    valtype::Type{V}
+    metapath::AbstractString
+    chunks::Vector{Chunk{K,V}}
+    lrucache::LRU
+end
+
+function ChunkedFile(metapath::AbstractString, K, V, max_cache)
+    chunks = Chunk{K,V}[]
+    meta = (filesize(metapath) == 0) ? Array(Any,0,0) : readcsv(metapath)
+    for idx in 1:size(meta,1)
+        fname = meta[idx,3]
+        push!(chunks, Chunk(fname, 0, filesize(fname), Int(meta[idx,1]):Int(meta[idx,2]), V))
+    end
+    ChunkedFile{K,V}(K, V, metapath, chunks, LRU(max_cache))
+end
+
+function writemeta(cf::ChunkedFile)
+    chunkpfx = splitext(cf.metapath)[1]
+    open(cf.metapath, "w") do meta
+        chunks = cf.chunks
+        idx = 1
+        for chunk in chunks
+            println(meta, first(chunk.keyrange), ",", last(chunk.keyrange), ",", chunkpfx, ".", idx)
+            idx += 1
+        end
+    end
+end
+
+ChunkedSparseMatrixCSC(metapath::AbstractString, max_cache::Int=5) = ChunkedFile(metapath, UnitRange{Int64}, SparseMatrixCSC{Float64,Int}, max_cache)
+ChunkedMemMappedMatrix(metapath::AbstractString, ncols::Int, max_cache::Int=5) = ChunkedFile(metapath, UnitRange{Int64}, MemMappedMatrix{Float64,ncols}, max_cache)
+
+function getchunk{K,V}(cf::ChunkedFile{K,V}, key::Int)
+    for chunk in cf.chunks
+        (key in chunk.keyrange) && return chunk
+    end
+    error("Key not found")
+end
+
+function data{K,SK,SV}(cf::ChunkedFile{K,SparseMatrixCSC{SK,SV}}, key::Int)
+    chunk = getchunk(cf, key)
+    d = data(chunk, cf.lrucache)
+    RecSys._sprowsvals(d, key - first(chunk.keyrange) + 1)
+end
+
+function data{K,T,N}(cf::ChunkedFile{K,MemMappedMatrix{T,N}}, key::Int)
+    M = data(getchunk(cf, key), cf.lrucache)
+    M.val
+end
